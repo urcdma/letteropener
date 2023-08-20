dd7e426c57859ef88e8b2587c7c363ae9d43bd21
hu <128804672+urcdma@users.noreply.github.com>
2023-08-20 19:49:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import logging
 from gui import GUI
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filename='app.log', filemode='w')
+
 def main():
+    logging.info("Application started.")
     gui = GUI()
     gui.start()
+    logging.info("Application closed.")
 
 if __name__ == "__main__":
     main()
